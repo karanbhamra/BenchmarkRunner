0e8b101d6d5d9bde68a9149060afb2fae70f69bd
Karan Bhamra <karan.bhamra@gmail.com>
2020-09-28 16:11:45 -0700

    Refactored out tests to their own class.

---
+++ Benchmarker/BenchmarkTests.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+
+namespace Benchmarker
+{
+    public class BenchmarkTests
+    {
+        [Benchmark]
+        public static void SimpleArrayLoop()
+        {
+            var arr = new int[1000];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * i;
+            }
+        }
+
+        private static List<string> testList;
+
+        [Benchmark]
+        public static void Simple()
+        {
+            int count = 10000000;
+
+            var list = new List<string>();
+            for (int i = 0; i < count; i++)
+                list.Add("hello");
+
+            testList = list;
+        }
+
+        [Benchmark]
+        public static void SimpleExpensive()
+        {
+            int count = 10000;
+
+            var list = new List<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string temp = "";
+
+                for (int j = 0; j < 1000; j++)
+                {
+                    temp += "a";
+                }
+
+                list.Add(temp);
+            }
+
+            for (int i = 0; i < count; i++)
+                list.Add("hello");
+
+            testList = list;
+        }
+    }
+}
